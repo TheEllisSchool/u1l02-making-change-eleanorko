b255be0550503bb140337eeb3378c968b8ce95fc
Eleanor BW <ebarthwu@gmail.com>
2019-09-06 11:46:46 -0400

    Now a caluclator with quarters, dimes, nickels, adn pennies. Program includes modulus

--- src/U1L02NumNickCalculator.java
+++ src/U1L02NumNickCalculator.java
@@ -12,10 +12,31 @@ public class U1L02NumNickCalculator {
 				
 				//System.out.println(UserMoney);
 				
+				int numQuart = userMoney / 25;
+				userMoney =  userMoney % 25;
+				int numDime = userMoney / 10;
+				userMoney = userMoney % 10;
 				int numNick = userMoney / 5;
-				int numPen = userMoney - (numNick * 5);
+				userMoney = userMoney % 5;
+				int numPen = userMoney;
+				JOptionPane.showMessageDialog(null, "You need " + numQuart + " quarter(s), " + numDime + 
+						" dime(s), " + numNick + " nickel(s) and " + numPen + " pennies");
 				
-				JOptionPane.showMessageDialog(null, "You need " + numNick + " nickels and " + numPen + " pennies");
+				
+				
+				
+				//int numNick = (userMoney - numPen) / 5;
+				//int numPen = userMoney % 5; 
+				//JOptionPane.showMessageDialog(null, "You need " + numPen + " pennies and " + numNick + " nickels");
+				
+				//If number is divisible by 25 then find number of quarters
+				//If number is divisible by 10 find number of dimes
+				//program should opperate in the order of most valued coins to least
+				
+				//int numNick = userMoney / 5;
+				//int numPen = userMoney - (numNick * 5);
+				
+				//JOptionPane.showMessageDialog(null, "You need " + numNick + " nickels and " + numPen + " pennies");
 
 
 	}
